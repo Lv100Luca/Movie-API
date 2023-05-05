71bc407195fca02188d74cbd51186d0e9086bdd3
Luca Diegel <Luca@trenz.de>
2023-05-05 10:11:12 +0200

    added Movie Exception

---
+++ SOFTW2_Uebungen/Models/Excxeption/MovieNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace SOFTW2_Uebungen.Models.Excxeption;
+
+public class MovieNotFoundException : Exception
+{
+    public MovieNotFoundException(): base("Movie not Found"){}
+
+    public MovieNotFoundException(string message)
+        : base(message)
+    {
+    }
+}
