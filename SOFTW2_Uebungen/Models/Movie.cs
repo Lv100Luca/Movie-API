1a9ce6de4bb9450bebce30fc1a926f1718a71245
Luca Diegel <Luca@trenz.de>
2023-05-05 10:07:37 +0200

    Changed variable names Added constructor without id

--- SOFTW2_Uebungen/Models/Movie.cs
+++ SOFTW2_Uebungen/Models/Movie.cs
@@ -4,10 +4,16 @@ public class Movie
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public Movie(int Id, string Name)
+    public Movie(int id, string name)
     {
-        this.Id = Id;
-        this.Name = Name;
+        this.Id = id;
+        this.Name = name;
+    }
+
+    public Movie(string name)
+    {
+        this.Name = name;
+        this.Id = -1;
     }
 
 }
